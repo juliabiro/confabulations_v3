26239124f9b19feb176c4688b50e412327227472
Julia Biro <julia.biro@gmail.com>
2017-03-25 23:53:34 +0100

    authetnications

--- confabulation/views.py
+++ confabulation/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World! This is Bozsis confabulation site")
+    if request.user.is_authenticated:
+        return HttpResponse("Hello, World! This is Bozsis TOP SECRET confabulations site")
+
+    else:
+        return defaultcontent()
+
+def defaultcontent(request):
+        return HttpResponse("Hello, World! This is Bozsis confabulation site")
+
