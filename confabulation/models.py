0108ef68b13ad46475d8071c953c7faf504e8dc3
Julia Biro <julia.biro@gmail.com>
2017-03-26 00:38:11 +0100

    ignore pyc files
    add nullable fields, remove all primary keys
    
    delete migrations

--- confabulation/models.py
+++ confabulation/models.py
@@ -5,78 +5,84 @@ from common.utils import ChoiceEnum
 
 
 class ParticipantTypes(ChoiceEnum):
-    student = 0
-    photographer = 1
-    non_photographer = 2
+    student = 'student'
+    photographer = 'photographer'
+    non_photographer = 'non-photgrapher'
 
 class AnalysisPoint(ChoiceEnum):
-    confabulation = 0
-    connection = 1
-    interaction = 2
-    emotion = 3
-    framing = 4
+    confabulation = 'confabulation'
+    connection = 'connection'
+    interaction = 'interaction'
+    emotion = 'emotion'
+    framing = 'framing'
 
 class Interactions(ChoiceEnum):
-    block = 0
-    analyze = 1
-    non_block_trigger = 2
-    respond_to_trigger = 3
-    audience = 4
+    block = 'block'
+    analyze = 'analyzer'
+    non_block_trigger = 'non-block trigger'
+    respond_to_trigger = 'respond to trigger'
+    audience = 'audience'
 
 class Emotions(ChoiceEnum):
-    trauma = 0
-    conflicted = 1
-    surprised = 3
+    trauma = 'traume'
+    conflicted = 'conflicted'
+    surprised = 'surprsied'
 
 class Framing(ChoiceEnum):
-    gray_matter = 0
-    identify_only = 1
-    unsecure_deflection = 2
-    connection = 1
-    theme  =  2
-    themeconnection = 3
-    themeSubject = 4
+    gray_matter = 'gray matter'
+    identify_only = 'identify only'
+    insecure_deflection = 'insecure deflection'
+    connection = 'connection'
+    theme  =  'theme'
+    themeconnection = 'themeconnection'
+    theme_ubject = 'theme subject'
 
 class Connections(ChoiceEnum):
-    story_to_story = 0
-    theme = 1
-    theme_to_theme = 2
-    theme_subject = 3
+    story_to_story = 'story to story connection'
+    theme = 'theme over multiple stories'
+    theme_to_theme = 'theme to theme connection'
+    theme_motif = 'common motif over multiple themes'
 
+class Gender(ChoiceEnum):
+    male = 'male'
+    female = 'female'
+    other = 'other'
 # Create your models here.
 
 class Participant(models.Model):
-    name = models.CharField(max_length=200, primary_key = True)
+    name = models.CharField(max_length=200)
     profile = models.CharField(max_length=2000)
-    participation_group = models.CharField(max_length=1, choices=ParticipantTypes.choices())
-    gender = models.IntegerField(default=0)
-    recording = models.OneToOneField('Recording', unique = True)
+    participation_group = models.CharField(max_length=50, choices=ParticipantTypes.choices())
+    gender = models.CharField(max_length=20, choices=Gender.choices())
+    recording = models.OneToOneField('Recording', null=True, blank=True, unique = True)
 
 
 class Recording(models.Model):
+    name = models.CharField(max_length = 100)
     date = models.DateField()
-    stories = models.ForeignKey('Story')
+    stories = models.ForeignKey('Story', null=True, blank = True)
     data_collection_circumstances = models.CharField(max_length = 2000)
-    sound_recording = models.FilePathField()
-    photos = models.ForeignKey("Photo")
+    sound_recording = models.FilePathField(null=True, blank=True)
+    photos = models.ForeignKey("Photo", null=True, blank=True)
 
 class Photo(models.Model):
     name = models.CharField(max_length = 50)
     file = models.FilePathField()
     duration = models.DurationField()
-    order_in_recording = models.IntegerField(unique= True) # marks the position of the photo in the recoding
+    order_in_recording = models.IntegerField(unique= True, null=True, blank=True) # marks the position of the photo in the recoding
 
 class Story(models.Model):
-    photo = models.ForeignKey('Photo')
-    transscribe_eng = models.CharField(max_length=10000)
-    transscribe_hu = models.CharField(max_length=10000)
-    video_url = models.FilePathField()
-    thumbnail = models.FilePathField()
-    analysis_points = models.CharField(max_length=1, choices = AnalysisPoint.choices())
-    emotions = models.CharField(max_length=1, choices = Emotions.choices())
-    interactions = models.CharField(max_length=1, choices = Interactions.choices())
-    framing = models.CharField(max_length=1, choices = Framing.choices())
-    connections = models.CharField(max_length=1, choices = Connections.choices())
+    name = models.CharField(max_length = 100)
+    photos = models.ForeignKey('Photo', null=True, blank=True)
+    transscribe_eng = models.CharField(max_length=10000, null=True, blank=True)
+    transscribe_hu = models.CharField(max_length=10000, null=True, blank=True)
+    video_url = models.FilePathField(null=True, blank=True)
+    thumbnail = models.FilePathField(null=True, blank=True)
+    analysis_points = models.CharField(max_length=50, choices = AnalysisPoint.choices())
+    emotions = models.CharField(max_length=50, choices = Emotions.choices(), null=True, blank=True)
+    interactions = models.CharField(max_length=50, choices = Interactions.choices(), null=True, blank=True)
+    framing = models.CharField(max_length=50, choices = Framing.choices(), null=True, blank=True)
+    connections = models.CharField(max_length=50, choices = Connections.choices(), null=True, blank=True)
 
 
 class StoryConnection(models.Model):
--- dbbackend/settings.py
+++ dbbackend/settings.py
@@ -126,9 +126,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+ 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 
 LOGIN_URL = '/login/'
+
