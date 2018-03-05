0133df6804cdf4429505fe60bf208365a630c8c3
Júlia Biró <julia.biro@gmail.com>
2018-03-05 21:03:12 +0100

    fix timestam

--- confabulation/migrations/0016_auto_20180305_1951.py
+++ confabulation/migrations/0016_auto_20180305_1951.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('confabulation', '0015_auto_20180305_1940'),
+        ('confabulation', '0015_auto_20180305_1951'),
     ]
 
     operations = [
