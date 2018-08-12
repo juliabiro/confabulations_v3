e337e0da5b2a096d5b73b4136623c564312a806a
Júlia Biró <julia.biro+git@contentful.com>
2018-08-13 00:04:48 +0200

    working test for a view (#6)
    * working test for a view
    
    needs modification in models too
    
    * made smart pylint just a bit smarter
    
    but: it is not catching syntax errors yet. they emit an error, but return code
    is not 1 or 2

--- confabulation/tests/test_views.py
+++ confabulation/tests/test_views.py
@@ -1,23 +1,22 @@
 from django.test import TestCase
 from django.test import Client
+from django.urls import reverse
 from confabulation.models import Story, Participant, ParticipantTypes, Gender
-
+from confabulation.views import *
 
 class ParticipantViewTestCase(TestCase):
     def setUp(self):
-        Participant.obkects.create(name="Test Bela",
-                                   profile="test profile",
-                                   participation_group=ParticipantTypes.photographer,
-                                   gender=Gender.female
-        )
+        self.participant = Participant(name="Test Bela",
+                                       profile="test profile",
+                                       participation_group=ParticipantTypes.photographer,
+                                       gender=Gender.female,
+                                       id=1)
+        self.participant.save()
         self.client = Client()
 
     def test_participant_view(self):
         """partipant view can be rendered"""
-        url = 'participant/1/'
-        response = self.client.get(url)
+        response = self.client.get(self.participant.get_absolute_url())
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'participantView.html')
+        self.assertTemplateUsed(response, 'confabulation/participantView.html')
         self.assertContains(response, 'Test Bela')
-        self.assertContains(response, 'Test Geza')
-
