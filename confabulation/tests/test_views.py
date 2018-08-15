18d8c7d6e689024344e1daad65f901dbb1bd51ef
Julia Biro <julia.biro@gmail.com>
2018-08-15 20:19:37 +0200

    tests for frontpage

--- confabulation/tests/test_views.py
+++ confabulation/tests/test_views.py
@@ -26,3 +26,35 @@ class ParticipantViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'confabulation/participantView.html')
         self.assertContains(response, 'Test Bela')
+
+class FrontPage(TestCase):
+    def setUp(self):
+        self.participant = Participant(name="Test Bela",
+                                       profile="test profile",
+                                       participation_group=ParticipantTypes.photographer,
+                                       gender=Gender.female,
+                                       id=1)
+        self.participant.save()
+
+        User = get_user_model()
+        User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+        self.client = Client()
+
+    def test_frontpage(self):
+        """frontpage loads with contents and css"""
+        self.client.login(username='temporary', password='temporary')
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'frontpage.html')
+        self.assertContains(response, 'Bözsike')
+
+        css_response = self.client.get('/static/css/style.css')
+        self.assertEqual(css_response.status_code, 200)
+
+    def test_frontpage_not_authenticated(self):
+        """unauthenticated users get redirected"""
+        response = self.client.get('/')
+        self.assertRedirects(response, '/login/?next=/')
