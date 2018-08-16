894b2369b76dbebf83fb54b8c5e29e1b1f7ec1dc
Julia Biro <julia.biro@gmail.com>
2018-08-16 23:28:52 +0200

    photos nicer in stroy page

--- confabulation/tests/test_views.py
+++ confabulation/tests/test_views.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
 from django.test import Client
 from django.contrib.auth import get_user_model
-from .db_data import *
+import mock
+import db_data
 from .utils_mock import mock_get_signed_asset_link
 from ..models import Participant, Story
-import mock
 
 class ParticipantView(TestCase):
     def setUp(self):
-        populate_db()
+        db_data.populate_db()
         self.participant = Participant.objects.get(pk=1)
         self.participant.save()
 
@@ -30,7 +30,7 @@ class ParticipantView(TestCase):
 @mock.patch('confabulation.views.get_signed_video_url', mock_get_signed_asset_link)
 class StoryView(TestCase):
     def setUp(self):
-        populate_db()
+        db_data.populate_db()
 
         User = get_user_model()
         User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
@@ -39,7 +39,7 @@ class StoryView(TestCase):
         self.client.login(username='temporary', password='temporary')
 
     def test_story_view(self):
-        story = Story.objects.get(pk=VALID_STORY_ID)
+        story = Story.objects.get(pk=db_data.VALID_STORY_ID)
         response = self.client.get(story.get_absolute_url())
 
         self.assertEqual(response.status_code, 200)
@@ -49,20 +49,20 @@ class StoryView(TestCase):
         self.assertContains(response, 'keyword1')
         self.assertContains(response, 'analysis_point1')
         self.assertContains(response, 'sometext')
-        self.assertContains(response, VALID_PHOTO_NAME)
-        self.assertContains(response, VALID_VIDEO_NAME)
+        self.assertContains(response, db_data.VALID_PHOTO_NAME)
+        self.assertContains(response, db_data.VALID_VIDEO_NAME)
 
     def test_story_view_invalid_media(self):
-        story = Story.objects.get(pk=INVALID_STORY_ID)
+        story = Story.objects.get(pk=db_data.INVALID_STORY_ID)
         response = self.client.get(story.get_absolute_url())
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'confabulation/storyView.html')
         self.assertContains(response, 'invalid notes')
-        self.assertContains(response, INVALID_PHOTO_NAME+" doesn&#39;t exist")
-        self.assertContains(response, MALFORMED_PHOTO_NAME+" doesn&#39;t exist")
-        self.assertContains(response, MISSING_PHOTO_NAME+" doesn&#39;t exist")
-        self.assertContains(response, INVALID_VIDEO_NAME+" doesn&#39;t exist")
+        self.assertContains(response, db_data.INVALID_PHOTO_NAME+" doesn&#39;t exist")
+        self.assertContains(response, db_data.MALFORMED_PHOTO_NAME+" doesn&#39;t exist")
+        self.assertContains(response, db_data.MISSING_PHOTO_NAME+" doesn&#39;t exist")
+        self.assertContains(response, db_data.INVALID_VIDEO_NAME+" doesn&#39;t exist")
 
 
 class FrontPage(TestCase):
