b04913b6dc81d0de38ed05e8812012c355fd6e3d
Julia Biro <julia.biro@gmail.com>
2018-11-06 23:24:46 +0100

    get themes and chains

--- confabulation/views/participant_views.py
+++ confabulation/views/participant_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import redirect
-from ..models import Participant, Story
+from ..models import Participant, Story, Chain, Theme
 from ..utils.s3_helpers import *
 from ..utils. media_helpers import get_story_thumb
 from .context_helpers import setup_page_context
@@ -15,12 +15,27 @@ def participants(request):
     setup_page_context(context)
     return render(request, 'confabulation/participants.html', context)
 
+def get_themes_for_stories(participant_stories):
+     for story in participant_stories:
+         themes = Theme.objects.filter(stories=story)
+         return themes
+
+def get_chains_for_themes(themes):
+    for theme in themes:
+        chains = Chain.objects.filter(themes=theme)
+        return chains
+
 def participant_view(request, participant_id):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
     participant = Participant.objects.get(pk=participant_id)
     participant_stories = Story.objects.filter(participant__id=participant_id).order_by('name')
-    context = {'participant':participant, 'stories': participant_stories}
+
+    themes = get_themes_for_stories(participant_stories)
+    chains = get_chains_for_themes(themes)
+
+
+    context = {'participant':participant, 'stories': participant_stories, 'themes': themes, 'chains': chains}
     setup_page_context(context)
     return render(request, 'confabulation/participantView.html', context)
