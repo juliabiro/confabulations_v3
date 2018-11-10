e9c70d5e271f97702272cad41faba5626c04d6ec
Júlia Biró <julia.biro+git@contentful.com>
2018-11-10 23:10:13 +0100

    Themes and chains 1 (#31)
    * get themes and chains
    
    * filters for chains and themes
    
    * theme and chain views
    
    * themes in participant view
    
    * get the right collections
    
    * te right kind of connections
    
    * supernice formatting
    
    * redo layout as Eszter wanted
    
    * display story to story connections
    
    * aslo display single stories if they exist
    
    * ordering in view is no longer by alphabet

---
+++ confabulation/utils/connection_helpers.py
@@ -0,0 +1,63 @@
+from ..models import Theme, Chain, Story, StoryToStoryConnection
+
+class ThemeWithStories():
+    def __init__ (self, theme, stories):
+        self.theme = theme
+        self.stories = stories
+
+class ChainWithThemes():
+    def __init__(self, chain, themes):
+        self.chain = chain
+        self.themes = themes
+
+def buildchains(participant_id, connection_range):
+    participant_chains = []
+    chains = Chain.objects.filter(themes__stories__participant_id=participant_id, connection_range=connection_range).distinct()
+
+    for chain in chains:
+        themes = chain.themes.all()
+
+        story_list = []
+        for theme in themes:
+            story_list.append(ThemeWithStories(theme, theme.stories.all()))
+
+        participant_chains.append(ChainWithThemes(chain, story_list))
+    return participant_chains
+
+def buildthemes(participant_id, connection_range):
+    participant_themes = []
+    themes = Theme.objects.filter(stories__participant_id=participant_id, connection_range=connection_range).distinct()
+    for theme in themes:
+        participant_themes.append(ThemeWithStories(theme, theme.stories.all()))
+
+    return participant_themes
+
+def buildstoryconnections(participant_id, connection_range):
+    participant_stories = []
+    story_pairs = StoryToStoryConnection.objects.filter(story1__participant_id=participant_id, connection_range=connection_range).distinct()
+    for sp in story_pairs:
+        participant_stories.append(sp.story1)
+        participant_stories.append(sp.story2)
+
+    return participant_stories
+
+def buildsinglestories(participant_id):
+    all_stories = Story.objects.filter(participant_id=participant_id).distinct()
+    themes = Theme.objects.filter(stories__participant_id=participant_id)
+    conns = StoryToStoryConnection.objects.filter(story1__participant_id=participant_id)
+
+    connected_stories = []
+    for theme in themes:
+        for s in theme.stories.distinct():
+            connected_stories.append(s.id)
+        for c in conns:
+            print(c)
+            connected_stories.append(c.story1.id)
+
+    print (connected_stories)
+    single_stories = []
+    for s in all_stories:
+        if s.id not in connected_stories:
+            single_stories.append(s)
+
+    return single_stories
--- confabulation/views/participant_views.py
+++ confabulation/views/participant_views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import redirect
-from ..models import Participant, Story
+from ..models import Participant, Story, Chain, Theme, ConnectionRange
 from ..utils.s3_helpers import *
 from ..utils. media_helpers import get_story_thumb
+from ..utils. connection_helpers import buildchains, buildthemes, buildstoryconnections, buildsinglestories
 from .context_helpers import setup_page_context
 
 def participants(request):
@@ -20,7 +21,31 @@ def participant_view(request, participant_id):
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
     participant = Participant.objects.get(pk=participant_id)
-    participant_stories = Story.objects.filter(participant__id=participant_id).order_by('name')
-    context = {'participant':participant, 'stories': participant_stories}
+    context = {'participant':participant}
+
+    intrachains = buildchains(participant_id, 'Intraconnection')
+    interchains = buildchains(participant_id, 'Interconnection')
+
+    chainless_themes = buildthemes(participant_id, 'Intraconnection')
+    story_connections_intra = buildstoryconnections(participant_id, "Intraconnection")
+    story_connections_inter = buildstoryconnections(participant_id, "Interconnection")
+
+    single_stories = buildsinglestories(participant_id)
+
+    if interchains:
+        context['interconnections'] = interchains
+    if intrachains:
+        context['intraconnections'] = intrachains
+    if chainless_themes:
+        context['chainless_themes'] = chainless_themes
+
+    if story_connections_intra:
+        context['story_connections_intra'] = story_connections_intra
+    if story_connections_inter:
+        context['story_connections_inter'] = story_connections_inter
+
+    if single_stories:
+        context['single_stories'] = single_stories
+
     setup_page_context(context)
     return render(request, 'confabulation/participantView.html', context)
