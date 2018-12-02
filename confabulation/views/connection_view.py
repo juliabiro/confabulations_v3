6fd187ff58427a7fb31d7c697e26d1ca6af1ccb1
Julia Biro <julia.biro@gmail.com>
2018-12-02 23:54:02 +0100

    show cains and themes in connection view

--- confabulation/views/connection_view.py
+++ confabulation/views/connection_view.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import redirect
 from ..models import Connection
 from ..utils.s3_helpers import *
+from ..utils.connection_helpers import ConnectionBuilder
 from .context_helpers import setup_page_context
 
 def connection_view(request, connection_id):
@@ -15,6 +16,11 @@ def connection_view(request, connection_id):
         'connection': connection
     }
 
+    connectionBuilder = ConnectionBuilder(connection.connection_range)
+    themes = connectionBuilder.buildthemes()
+    chains = connectionBuilder.buildchains()
+    context['themes'] = themes
+    context['chains'] = chains
     setup_page_context(context)
     return render(request, 'confabulation/connectionView.html', context)
 
