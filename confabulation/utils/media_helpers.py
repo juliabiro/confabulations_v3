258365d0ae81adff9080c010b2f0cd8c4d2ea79e
Julia Biro <julia.biro@gmail.com>
2018-12-07 20:30:05 +0100

    template tag for showing story thumb and link

--- confabulation/utils/media_helpers.py
+++ confabulation/utils/media_helpers.py
@@ -6,21 +6,15 @@ from .s3_helpers import *
 from ..models import Story
 from .data import CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, THUMBNAILS_PATH
 
-def get_cloudinary_image_thumb(key):
+def get_cloudinary_image_thumb(key, size):
     cloudinary.config(
     cloud_name = "dpn5pmgin",
     api_key = CLOUDINARY_API_KEY,
     api_secret = CLOUDINARY_API_SECRET
     )
 
-    url=cloudinary.CloudinaryImage(THUMBNAILS_PATH +key+'.jpg').build_url( width=100, sign_url=True)
+    url=cloudinary.CloudinaryImage(THUMBNAILS_PATH +key+'.jpg').build_url( width=size, sign_url=True)
     return url
 
-def get_story_thumb(story):
-    return get_cloudinary_image_thumb(story.name)
-
-class StoryWithThumbnail():
-    def __init__(self, story):
-        self.name = story.name
-        self.url = story.get_absolute_url()
-        self.thumbnail = get_story_thumb(story)
+def get_story_thumb(story, size):
+    return get_cloudinary_image_thumb(story.name, size)
