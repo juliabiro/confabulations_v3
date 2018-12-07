f2b729418defd9f0cda3bb13b390f38e6f9a65d8
Júlia Biró <julia.biro+git@contentful.com>
2018-12-07 23:40:51 +0100

    new template tags (#47)
    template tags for displaying theme chain and story collections. reusing
    each other, allowing for more flexible repetition.

---
+++ confabulation/templatetags/story_to_story_connection_list.py
@@ -0,0 +1,14 @@
+from django import template
+from ..models import Story
+from django.utils.html import format_html_join
+
+
+register = template.Library()
+
+@register.simple_tag
+def story_to_story_connection_list(story_pairs):
+    return format_html_join('',
+                            "<li><a href='{}'>{}</a> - <a href='{}'>{}</a></li>",
+        ((s.story1.get_absolute_url(), s.story1.name,s.story2.get_absolute_url(), s.story2.name) for s in story_pairs)
+    )
+
---
+++ confabulation/templatetags/theme_with_story_list.py
@@ -0,0 +1,14 @@
+from django import template
+from django.utils.html import format_html_join
+from .story_list import story_list
+
+
+register = template.Library()
+
+@register.simple_tag
+def theme_with_story_list(themes):
+    return format_html_join('',
+                            "<div class='tc_block'><h5><a href='{}'>{}</a></h5>{}</div>",
+        ((t.theme.get_absolute_url(), t.theme.name, story_list(t.stories)) for t in themes)
+    )
+
