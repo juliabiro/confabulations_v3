e5fa758fedc4d263b1fbaa291b5ce4b91d6b70ed
Julia Biro <julia.biro@gmail.com>
2018-12-08 20:21:34 +0100

    color codes for themes chains and connections

--- confabulation/templatetags/chain_with_theme_list.py
+++ confabulation/templatetags/chain_with_theme_list.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.simple_tag
 def chain_with_theme_list(chains):
     return format_html_join('',
-                            "<div class='tc_block'><h5><a href='{}'>{}</a></h5>{}</div>",
+                            "<div class='tc_block'><div class='chain'><h5><a href='{}'>{}</a></h5></div>{}</div>",
         ((c.chain.get_absolute_url(), c.chain.name, theme_list(c.themes)) for c in chains)
     )
 
