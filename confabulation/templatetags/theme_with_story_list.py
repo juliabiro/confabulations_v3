e5f6f6694ab3df2c6b11aedabdcebbcb24115129
Júlia Biró <julia.biro+git@contentful.com>
2019-02-02 23:46:29 +0100

    connections sidebar fixes (#63)
    * expandable story list 1
    
    there is a bug: some submenus dont open. they are the ones that have "'" in them
    fix: ids cant start with numbers!
    
    * stop trembling in sidebar by fixing tighter arrangement
    
    * tighter arrangement, toggle inlien with theme

--- confabulation/templatetags/theme_with_story_list.py
+++ confabulation/templatetags/theme_with_story_list.py
@@ -5,10 +5,15 @@ from .story_list import story_list
 
 register = template.Library()
 
+def submenu_item_name(string):
+    # make a referrable html id from the theme name.
+    # remove charcters that are incompatible
+    return "submenu_"+string.replace(" ", "").replace("/","").replace("'","")
+
 @register.simple_tag
 def theme_with_story_list(themes):
     return format_html_join('',
-                            "<div class='tc_block'><h5><a href='{}' class='theme'>{}</a></h5>{}</div>",
-        ((t.theme.get_absolute_url(), t.theme.name, story_list(t.stories)) for t in themes)
+                            "<div class='tc_block'> <h5>  <a href='{}' class='theme'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a> </h5> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
+                            ((t.theme.get_absolute_url(), t.theme.name,submenu_item_name(t.theme.name),  submenu_item_name(t.theme.name), story_list(t.stories)) for t in themes)
     )
 
