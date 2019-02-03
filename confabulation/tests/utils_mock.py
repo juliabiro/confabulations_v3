5247d50c2c38693dca690f1c85515fe639a7d4d6
Julia Biro <julia.biro@gmail.com>
2019-02-03 23:50:25 +0100

    fix test

--- confabulation/tests/utils_mock.py
+++ confabulation/tests/utils_mock.py
@@ -3,7 +3,8 @@ from ..utils.data import TAXONOMY_VIDEO_KEY
 
 def mock_get_signed_asset_link(key, raise_error=True):
     if key == TAXONOMY_VIDEO_KEY:
-        return "http://taxonomy_video_url/"
+        return "https://taxonomy_video_url/"
+
     if key in [VALID_VIDEO_NAME, VALID_VIDEO_NAME]:
         return "https://valid_url/TEST/"+key
     if raise_error:
--- confabulation/views/static_views.py
+++ confabulation/views/static_views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import redirect
 from .context_helpers import setup_page_context
-from ..utils.s3_helpers import get_signed_video_url, parse_key_from_url
+from ..utils.s3_helpers import get_signed_video_url
 from ..utils.data import TAXONOMY_VIDEO_KEY
 
 # Create your views here.
 def index(request):
     video_url = TAXONOMY_VIDEO_KEY
-    url = get_signed_video_url(parse_key_from_url(video_url))
+    url = get_signed_video_url(video_url)
     context = {}
     context['video_url'] = url
     return render(request, 'frontpage.html', context)
