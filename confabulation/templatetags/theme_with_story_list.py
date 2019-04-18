7afdf31194a94d3ddfc51013609762cd2f0a3ca6
Julia Biro <julia.biro@gmail.com>
2019-04-18 22:23:40 +0200

    yellow background for chains and themes

--- confabulation/templatetags/theme_with_story_list.py
+++ confabulation/templatetags/theme_with_story_list.py
@@ -11,9 +11,9 @@ def submenu_item_name(string):
     return "submenu_"+string.replace(" ", "").replace("/","").replace("'","")
 
 @register.simple_tag
-def theme_with_story_list(themes):
+def theme_with_story_list(themes, connection_range):
     return  format_html_join('',
-                            "<div class='tc_block'> <h5>  <a href='{}' class='theme'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a> </h5> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
-                            ((t.theme.get_absolute_url(), t.theme.name,submenu_item_name(t.theme.name),  submenu_item_name(t.theme.name), story_list(t.stories)) for t in themes)
+                             "<div class='tc_block'> <h5>  <a href='{}' class='theme {}'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a> </h5> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
+                            ((t.theme.get_absolute_url(), connection_range, t.theme.name,submenu_item_name(t.theme.name),  submenu_item_name(t.theme.name), story_list(t.stories)) for t in themes)
     )
 
