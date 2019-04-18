196f78860fcffdded7329c840fe58f3fb387748a
Júlia Biró <julia.biro+git@contentful.com>
2019-04-18 22:30:50 +0200

    yellow background for chains and themes (#70)

--- confabulation/templatetags/chain_with_theme_list.py
+++ confabulation/templatetags/chain_with_theme_list.py
@@ -6,9 +6,9 @@ from .theme_list import theme_list
 register = template.Library()
 
 @register.simple_tag
-def chain_with_theme_list(chains):
+def chain_with_theme_list(chains, connection_range):
     return format_html_join('',
-                            "<div class='tc_block'><h5><a href='{}' class='chain'>{}</a></h5>{}</div>",
-        ((c.chain.get_absolute_url(), c.chain.name, theme_list(c.themes)) for c in chains)
+                            "<div class='tc_block'><h5><a href='{}' class='chain {}'>{}</a></h5>{}</div>",
+        ((c.chain.get_absolute_url(), connection_range, c.chain.name, theme_list(c.themes)) for c in chains)
     )
 
