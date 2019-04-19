7f1d61189da7d9dd5d192489e904b8e09149dae9
Julia Biro <julia.biro@gmail.com>
2019-04-19 22:09:32 +0200

    vertical arrangement in theme list in sidebar

--- confabulation/templatetags/theme_list.py
+++ confabulation/templatetags/theme_list.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.simple_tag
 def theme_list(themes, connection_range):
     return format_html_join('',
-                            "<a href='{}' class='theme {}'>{}</a>",
+                            "<div class='tc_block'><a href='{}' class='theme {}'>{}</a></div>",
         ((t.theme.get_absolute_url(), connection_range, t.theme.name) for t in themes)
     )
 
