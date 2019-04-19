4632647a1531420ca9dc1d74e03116fe9e51abe1
Júlia Biró <julia.biro+git@contentful.com>
2019-04-19 20:56:18 +0200

    tidier sidebar (#73)
    * tify up connection listing titles
    
    * remove uneccesary parts

--- confabulation/templatetags/theme_list.py
+++ confabulation/templatetags/theme_list.py
@@ -5,9 +5,9 @@ from django.utils.html import format_html_join
 register = template.Library()
 
 @register.simple_tag
-def theme_list(themes):
+def theme_list(themes, connection_range):
     return format_html_join('',
-                            "<a href='{}' class='theme'>{}</a>",
-        ((t.theme.get_absolute_url(), t.theme.name) for t in themes)
+                            "<a href='{}' class='theme {}'>{}</a>",
+        ((t.theme.get_absolute_url(), connection_range, t.theme.name) for t in themes)
     )
 
