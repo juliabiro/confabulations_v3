021fe63e88bb0646ca9d1b4a7fc93a0bef0b326a
Júlia Biró <julia.biro+git@contentful.com>
2019-04-20 00:09:59 +0200

    sidebar all themes listed the same way (#74)
    * remove unused templatetag
    
    * vertical arrangement in theme list in sidebar
    
    * unified look in sidebar
    
    * really much prettier sidebar
    
    * some resizing in the page

--- confabulation/templatetags/chain_with_theme_list.py
+++ confabulation/templatetags/chain_with_theme_list.py
@@ -1,14 +1,19 @@
 from django import template
 from django.utils.html import format_html_join
-from .theme_list import theme_list
+from .theme_with_story_list import theme_with_story_list
 
 
 register = template.Library()
 
+def submenu_item_name(string):
+    # make a referrable html id from the theme name.
+    # remove charcters that are incompatible
+    return "submenu_"+string.replace(" ", "").replace("/","").replace("'","")
+
 @register.simple_tag
 def chain_with_theme_list(chains, connection_range):
     return format_html_join('',
-                            "<div class='tc_block'><h5><a href='{}' class='chain {}'>{}</a></h5>{}</div>",
-        ((c.chain.get_absolute_url(), connection_range, c.chain.name, theme_list(c.themes, connection_range)) for c in chains)
+                             "<div class='chain {}' > <h5>  <a href='{}'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a> </h5> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
+        ((connection_range, c.chain.get_absolute_url(), c.chain.name, submenu_item_name(c.chain.name), submenu_item_name(c.chain.name), theme_with_story_list(c.themes, connection_range)) for c in chains)
     )
 
