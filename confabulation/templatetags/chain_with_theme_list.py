cdc623f4cb0e06e7f87f84f83260dbcf307492bb
Julia Biro <julia.biro@gmail.com>
2019-04-19 23:04:25 +0200

    unified look in sidebar

--- confabulation/templatetags/chain_with_theme_list.py
+++ confabulation/templatetags/chain_with_theme_list.py
@@ -1,6 +1,6 @@
 from django import template
 from django.utils.html import format_html_join
-from .theme_list import theme_list
+from .theme_with_story_list import theme_with_story_list
 
 
 register = template.Library()
@@ -13,8 +13,7 @@ def submenu_item_name(string):
 @register.simple_tag
 def chain_with_theme_list(chains, connection_range):
     return format_html_join('',
-                             "<div class='tc_block'> <h5>  <a href='{}' class='theme {}'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a> </h5> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
-                            #"<div class='tc_block'><h5><a href='{}' class='chain {}'>{}</a></h5>{}</div>",
-        ((c.chain.get_absolute_url(), connection_range, c.chain.name, submenu_item_name(c.chain.name), submenu_item_name(c.chain.name), theme_list(c.themes, connection_range)) for c in chains)
+                             "<div class='chain {}' > <h5>  <a href='{}'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a> </h5> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
+        ((connection_range, c.chain.get_absolute_url(), c.chain.name, submenu_item_name(c.chain.name), submenu_item_name(c.chain.name), theme_with_story_list(c.themes, connection_range)) for c in chains)
     )
 
