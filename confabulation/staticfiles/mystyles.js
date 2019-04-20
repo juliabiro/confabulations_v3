fe66a5ad0c00e3e6f257f58edfd970a6b50958a0
Julia Biro <julia.biro@gmail.com>
2019-04-20 04:17:04 +0200

    really pretty sidebars and arrangement

--- confabulation/staticfiles/mystyles.js
+++ confabulation/staticfiles/mystyles.js
@@ -1,25 +1,10 @@
 $(document).ready(function () {
 
-  $('#sidebarLeftCollapse').on('click', function () {
-    $('#sidebar_left_bitton').toggleClass('active');
+  $('#sidebar_left_button').on('change', function () {
     $('#sidebar_left').toggleClass('active');
-
-    if ($('#sidebar_left_button')[0].classList.contains('active')) {
-      $(this).text("Show left sidebar");
-    } else {
-      $(this).text("Hide left sidebar");
-    }
   });
-
-  $('#sidebarRightCollapse').on('click', function () {
-    $('#sidebar_right_button').toggleClass('active');
+  $('#sidebar_right_button').on('change', function () {
     $('#sidebar_right').toggleClass('active');
-
-    if ($('#sidebar_right_button')[0].classList.contains('active')) {
-      $('#sidebarRightCollapse').text("Show right sidebar");
-    } else {
-      $('#sidebarRightCollapse').text("Hide right sidebar");
-    }
   });
 
 });
