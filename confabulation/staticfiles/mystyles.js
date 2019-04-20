98e34784d654274a41abca6823434d185d29b0bc
Julia Biro <julia.biro@gmail.com>
2019-04-20 02:33:10 +0200

    smarter sidebar buttons

--- confabulation/staticfiles/mystyles.js
+++ confabulation/staticfiles/mystyles.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
   $('#sidebarLeftCollapse').on('click', function () {
+    $('#sidebar_left_bitton').toggleClass('active');
     $('#sidebar_left').toggleClass('active');
 
-    if ($('#sidebar_left')[0].classList.contains('active')) {
+    if ($('#sidebar_left_button')[0].classList.contains('active')) {
       $(this).text("Show left sidebar");
     } else {
       $(this).text("Hide left sidebar");
@@ -11,9 +12,10 @@ $(document).ready(function () {
   });
 
   $('#sidebarRightCollapse').on('click', function () {
+    $('#sidebar_right_button').toggleClass('active');
     $('#sidebar_right').toggleClass('active');
 
-    if ($('#sidebar_right')[0].classList.contains('active')) {
+    if ($('#sidebar_right_button')[0].classList.contains('active')) {
       $('#sidebarRightCollapse').text("Show right sidebar");
     } else {
       $('#sidebarRightCollapse').text("Hide right sidebar");
