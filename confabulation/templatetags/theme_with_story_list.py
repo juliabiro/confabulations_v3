71ef28c8fc091c156cb1322440d6cdc27c4f978a
Júlia Biró <julia.biro@gmail.com>
2019-04-25 20:11:26 +0200

    highligth stories on participant page that belong to a theme (#82)

--- confabulation/templatetags/theme_with_story_list.py
+++ confabulation/templatetags/theme_with_story_list.py
@@ -13,7 +13,7 @@ def submenu_item_name(string):
 @register.simple_tag
 def theme_with_story_list(themes, connection_range):
     return  format_html_join('',
-                             "<div class='theme {}'> <h6><a href='{}'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle\"></a></h6> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
-                            ((connection_range, t.theme.get_absolute_url(), t.theme.name,submenu_item_name(t.theme.name),  submenu_item_name(t.theme.name), story_list(t.stories)) for t in themes)
+                             "<div class='theme {}' > <h6><a href='{}'>{}</a> <a href=\"#{}\" data-toggle=\"collapse\" aria-expanded=\"false\" class=\"dropdown-toggle theme-dropdown\" id='theme{}'></a></h6> <div class=\"collapse\" id=\"{}\">{} </div> </div>",
+                            ((connection_range, t.theme.get_absolute_url(), t.theme.name,submenu_item_name(t.theme.name), t.theme.id, submenu_item_name(t.theme.name),  story_list(t.stories)) for t in themes)
     )
 
