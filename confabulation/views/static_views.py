4dfde8a844a035b89f6047ea738d20600184a017
Julia Biro <julia.biro@gmail.com>
2019-04-27 18:02:49 +0200

    better frontpage

--- confabulation/views/static_views.py
+++ confabulation/views/static_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import redirect
 from .context_helpers import setup_page_context
+from ..models import AnalysisPoint
 from ..utils.s3_helpers import get_signed_video_url
 from ..utils.data import TAXONOMY_VIDEO_KEY
 
@@ -11,6 +12,17 @@ def index(request):
     url = get_signed_video_url(video_url)
     context = {}
     context['video_url'] = url
+
+    try:
+        confabulation = AnalysisPoint.objects.filter(name="Confabulation")[0]
+        context['confabulation_url'] = confabulation.get_absolute_url()
+        going_beyond=AnalysisPoint.objects.filter(name="Going Beyond")[0]
+        context['going_beyond_url'] = going_beyond.get_absolute_url()
+
+    except Exception as e:
+        print(e)
+
+    setup_page_context(context, navbar=False)
     return render(request, 'frontpage.html', context)
 
 def about(request):
