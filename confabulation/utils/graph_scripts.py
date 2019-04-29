1ced216def46321f86c0197a0d675e2433db7477
Julia Biro <julia.biro@gmail.com>
2019-04-29 20:34:14 +0200

    separate data collection and script data generation

--- confabulation/utils/graph_scripts.py
+++ confabulation/utils/graph_scripts.py
@@ -26,62 +26,60 @@ def sanitize_name(name):
     return name.replace('/', '__').replace("'","-")
 
 def chain_node(participant, chain):
-    node = Template("{id: $id, label:'$label', url: '$url', group: 'chain', color: '$color',size: 50, mass: 0.1}").substitute(id=get_unique_node_id(chain), label=sanitize_name(chain.name), url=chain.get_absolute_url(), color=COLORS[str(participant.id)])
+    node = Template("{ id: $id, label:'$label', url: '$url', group: 'chain', color: '$color',size: 50, mass: 0.1 }").substitute(id=get_unique_node_id(chain), label=sanitize_name(chain.name), url=chain.get_absolute_url(), color=COLORS[str(participant.id)])
     return node
 
 def theme_node(participant, theme):
-    node = Template("{id: $id, label:'$label', url: '$url', group: 'theme', color: '$color',size: 25, mass:0.2}").substitute(id=get_unique_node_id(theme), label=sanitize_name(theme.name), url=theme.get_absolute_url(), color=COLORS[str(participant.id)])
+    node = Template("{ id: $id, label:'$label', url: '$url', group: 'theme', color: '$color',size: 25, mass:0.2 }").substitute(id=get_unique_node_id(theme), label=sanitize_name(theme.name), url=theme.get_absolute_url(), color=COLORS[str(participant.id)])
     return node
 
 def story_node(participant, story):
-    node = Template("{ id: $id, label: '$label', url: '$url', group: 'story_$group',size:12, mass:0.5}").substitute(id=get_unique_node_id(story), label=story.name, url=story.get_absolute_url(), group=participant.name.replace(' ','_'))
+    node = Template("{ id: $id, label: '$label', url: '$url', group: 'story_$group',size:12, mass:0.5 }").substitute(id=get_unique_node_id(story), label=story.name, url=story.get_absolute_url(), group=participant.name.replace(' ','_'))
     return node
 
 def participant_node(participant):
-    node = Template("{id: $id, label:'$label', url: '$url', group: 'participant_$group', color: '$color', size: 75, mass: 0.01}").substitute(id=get_unique_node_id(participant), label=participant.name, url=participant.get_absolute_url(), color=COLORS[str(participant.id)], group=participant.name.replace(' ','_'))
+    node = Template("{ id: $id, label:'$label', url: '$url', group: 'participant_$group', color: '$color', size: 75, mass: 0.01 }").substitute(id=get_unique_node_id(participant), label=participant.name, url=participant.get_absolute_url(), color=COLORS[str(participant.id)], group=participant.name.replace(' ','_'))
     return node
 
 def story_to_story_edge(story1, story2):
-    edge= Template("{to: $to, from:$fromm}").substitute(to=get_unique_node_id(story1), fromm=get_unique_node_id(story2))
+    edge= Template("{ to: $to, from:$fromm }").substitute(to=get_unique_node_id(story1), fromm=get_unique_node_id(story2))
     return edge
 
 def story_to_participant_edge(participant, story):
-    edge= Template("{to: $to, from:$fromm}").substitute(to=get_unique_node_id(story), fromm=get_unique_node_id(participant))
+    edge= Template("{ to: $to, from:$fromm }").substitute(to=get_unique_node_id(story), fromm=get_unique_node_id(participant))
     return edge
 
 def story_to_theme_edge(theme, story):
-    edge= Template("{to: $to, from:$fromm}").substitute(to=get_unique_node_id(story), fromm=get_unique_node_id(theme))
+    edge= Template("{ to: $to, from:$fromm }").substitute(to=get_unique_node_id(story), fromm=get_unique_node_id(theme))
     return edge
 
 def theme_to_chain_edge(theme, chain):
-    edge= Template("{to: $to, from:$fromm}").substitute(to=get_unique_node_id(chain), fromm=get_unique_node_id(theme))
+    edge= Template("{ to: $to, from:$fromm }").substitute(to=get_unique_node_id(chain), fromm=get_unique_node_id(theme))
     return edge
 
 def story_group(participant=None):
     color = COLORS[(str(participant.id))] if participant is not None else 'red'
-    return Template("story_$name: { color: '$color',font: '25px arial black', shape: 'dot',}").substitute(name=participant.name.replace(" ", "_"), color=color)
+    return Template("story_$name: { color: '$color',font: '25px arial black', shape: 'dot'}").substitute(name=participant.name.replace(" ", "_"), color=color)
 
 def theme_group(participant=None):
     color = COLORS[(str(participant.id))] if participant is not None else 'aqua'
-    return Template("theme: { color: '$color',font: '25px arial black', shape: 'triangle',}").substitute( color=color)
+    return Template("theme: { color: '$color',font: '25px arial black', shape: 'triangle' }").substitute( color=color)
 
 def chain_group(participant=None):
     color = COLORS[(str(participant.id))] if participant is not None else 'maroon'
-    return Template("chain: { color: '$color',font: '25px arial black', shape: 'hexagon',}").substitute(color=color)
+    return Template("chain: { color: '$color',font: '25px arial black', shape: 'hexagon' }").substitute(color=color)
 
 def participant_group(participant):
-    return Template("participant_$name: {color: '$color', font: '25px arial black', shape: 'ellipse',}").substitute(name=participant.name.replace(' ','_'), color=COLORS[str(participant.id)])
+    return Template("participant_$name: { color: '$color', font: '25px arial black', shape: 'ellipse' }").substitute(name=participant.name.replace(' ','_'), color=COLORS[str(participant.id)])
 
 def data_to_script(data):
     return ',\n'.join(list(set(data)))
 
 # collector functions
-def story_to_participant_edges(participant):
+def collect_story_to_participant_edges(participant):
     stories = Story.objects.filter(participant__id=participant.id).distinct()
 
-    edges = data_to_script([
-        story_to_participant_edge(participant, s) for s in stories
-    ])
+    edges = [story_to_participant_edge(participant, s) for s in stories]
 
     return edges
 
@@ -95,9 +93,9 @@ def collect_participant_story_connections(participant):
     node_list = [story_node(participant, s) for s in stories]
 
     edge_list = [story_to_story_edge(p.story1, p.story2) for p in pairs]
-    group= story_group(participant)
+    groups= [story_group(participant)]
 
-    return node_list, edge_list, group
+    return node_list, edge_list, groups
 
 
 def participant_story_connections(participant):
--- confabulation/views/graph_view.py
+++ confabulation/views/graph_view.py
@@ -14,23 +14,20 @@ def graph_participant_view(request, participant_id):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
-    context={
-        'participant_graphs':[],
-    }
     participant=get_object_or_404(Participant, pk=participant_id)
 
 
     #node_list, edge_list, group = participant_story_connections(participant)
     # instead get the whole package:
 
-    node_list, edge_list, groups = participant_chains_themes_stories(participant)
+    node_list, edge_list, groups = collect_participant_chains_themes_stories(participant)
 
-    context['participant_graphs'].append({
-        'name': participant.name.replace(' ','_'),
-        'nodes': node_list,
-        'edges': edge_list,
-        'groups': groups,
-    })
+    context = {
+        'name': sanitize_name(participant.name),
+        'nodes': data_to_script(node_list),
+        'edges': data_to_script(edge_list),
+        'groups': data_to_script(groups),
+    }
 
     setup_page_context(context)
     return render(request, 'confabulation/participantGraph.html', context)
@@ -39,27 +36,25 @@ def graph_view(request):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
-    context={
-        'participant_graphs':[],
-        'story_to_participant_graph':{
-            'nodes':"",
-            'edges':"",
-        }
-    }
+
+    nodes=[]
+    edges=[]
+    groups=[]
     participants= Participant.objects.distinct()
     for participant in participants:
-        node_list, edge_list, group = participant_story_connections(participant)
+        n, e, g = collect_participant_story_connections(participant)
+        nodes.extend(n)
+        edges.extend(e)
+        groups.extend(g)
+        nodes.append(participant_node(participant))
+        edges.extend(collect_story_to_participant_edges(participant))
+        groups.append(story_group(participant))
 
+    context={}
+    context['nodes'] = data_to_script(nodes)
+    context['edges'] = data_to_script(edges)
+    context['groups'] = data_to_script(groups)
 
-        context['participant_graphs'].append({
-            'name': participant.name.replace(' ','_'),
-            'nodes': node_list,
-            'edges': edge_list,
-            'groups': ','.join([group, story_group(participant)])
-        })
-
-    context['story_to_participant_graph']['nodes']=','.join([participant_node(p) for p in participants])
-    context['story_to_participant_graph']['edges']=','.join([story_to_participant_edges(p) for p in participants])
 
     setup_page_context(context)
     return render(request, 'confabulation/graphView.html', context)
