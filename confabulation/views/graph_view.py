4c80b3df85c83953ecc56f4b2a473637f0576d4b
Julia Biro <julia.biro@gmail.com>
2019-05-25 14:30:08 +0200

    static view for big graph with doanloadable image

--- confabulation/views/graph_view.py
+++ confabulation/views/graph_view.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.shortcuts import redirect, get_object_or_404
 from ..models import  Story, Era, AnalysisPoint, Participant, Keyword, StoryToStoryConnection, Theme
 # from ..utils.s3_helpers import *
-from ..utils.media_helpers import get_image_url
+from ..utils.media_helpers import get_image_url, get_graph_url
 from ..utils.connection_helpers import ParticipantConnectionBuilder
 from .context_helpers import setup_page_context
 from ..utils.story_sorter import sort_story_list
@@ -42,23 +42,27 @@ def graph_view(request):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
-
-    nodes=[]
-    edges=[]
-    groups=[]
-    participants= Participant.objects.distinct()
-    for participant in participants:
-        n, e, g = collect_participant_chains_themes_stories(participant)
-        nodes.extend(n)
-        edges.extend(e)
-        groups.extend(g)
-        groups.append(story_group(participant))
+    static = request.GET.get('static')
 
     context={}
+    if static:
+        context={'static': True, 'big_graph_url': get_graph_url(size=3000)}
+    else:
+        nodes=[]
+        edges=[]
+        groups=[]
+        participants= Participant.objects.distinct()
+        for participant in participants:
+            n, e, g = collect_participant_chains_themes_stories(participant)
+            nodes.extend(n)
+            edges.extend(e)
+            groups.extend(g)
+            groups.append(story_group(participant))
 
-    context['nodes'] = data_to_script(nodes)
-    context['edges'] = data_to_script(edges)
-    context['groups'] = data_to_script(groups)
+
+        context['nodes'] = data_to_script(nodes)
+        context['edges'] = data_to_script(edges)
+        context['groups'] = data_to_script(groups)
 
     setup_legend(context)
     setup_page_context(context)
