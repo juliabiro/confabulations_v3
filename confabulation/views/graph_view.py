2a3ee63047c4352134131ec02bef7c478873e66d
Julia Biro <julia.biro@gmail.com>
2019-05-25 13:38:15 +0200

    better legend

--- confabulation/views/graph_view.py
+++ confabulation/views/graph_view.py
@@ -10,6 +10,12 @@ from ..utils.story_sorter import sort_story_list
 from ..utils.connection_helpers import ConnectionBuilder
 from ..utils.graph_scripts import *
 
+def setup_legend(context):
+    context['legend']={
+        'chain': get_image_url('confabulations/graphs/chain.png', 60),
+        'theme': get_image_url('confabulations/graphs/theme.png', 40),
+        'story': get_image_url('confabulations/graphs/story.png', 30),
+        'inter': get_image_url('confabulations/graphs/inter.png', 40)}
 
 def graph_participant_view(request, participant_id):
     if not request.user.is_authenticated:
@@ -18,11 +24,8 @@ def graph_participant_view(request, participant_id):
     participant=get_object_or_404(Participant, pk=participant_id)
 
 
-    #node_list, edge_list, group = participant_story_connections(participant)
-    # instead get the whole package:
-
-    # node_list, edge_list, groups = collect_participant_chains_themes_stories(participant)
-    node_list, edge_list, groups = legend(participant)
+    node_list, edge_list, groups = collect_participant_chains_themes_stories(participant)
+    # node_list, edge_list, groups = legend(participant)
 
     context = {
         'name': sanitize_name(participant.name),
@@ -31,11 +34,7 @@ def graph_participant_view(request, participant_id):
         'groups': data_to_script(groups),
     }
 
-    context['legend']={
-        'chain': get_image_url('confabulations/graphs/chain.png', 50),
-        'theme': get_image_url('confabulations/graphs/theme.png', 50),
-        'story': get_image_url('confabulations/graphs/story.png', 50),
-        'inter': get_image_url('confabulations/graphs/inter.png', 50)}
+    setup_legend(context)
     setup_page_context(context)
     return render(request, 'confabulation/participantGraph.html', context)
 
@@ -61,12 +60,7 @@ def graph_view(request):
     context['edges'] = data_to_script(edges)
     context['groups'] = data_to_script(groups)
 
-    context['legend']={
-        'chain': get_image_url('confabulations/graphs/chain.png', 50),
-        'theme': get_image_url('confabulations/graphs/theme.png', 50),
-        'story': get_image_url('confabulations/graphs/story.png', 50),
-        'inter': get_image_url('confabulations/graphs/inter.png', 50)}
-
+    setup_legend(context)
     setup_page_context(context)
     return render(request, 'confabulation/graphView.html', context)
 
