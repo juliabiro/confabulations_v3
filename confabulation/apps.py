f78af8629abea920fbdef5bfa8fd1228548a7e4b
Júlia Biró <julia.biro+git@gmail.com>
2020-12-20 22:26:42 +0100

    instrument with beeline (#110)

--- confabulation/apps.py
+++ confabulation/apps.py
@@ -1,7 +1,20 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.apps import AppConfig
+import beeline
 
+
+HONEYCOMB_API_KEY=settings.HONEYCOMB_API_KEY
 
 class ConfabulationConfig(AppConfig):
     name = 'confabulation'
+    def ready(self):
+        # If you use uwsgi, gunicorn, celery, or other pre-fork models, see the section below on pre-fork
+        # models and do not initialize here.
+            beeline.init(
+                writekey=HONEYCOMB_API_KEY,
+                dataset='confabulations-v3',
+                service_name='confabulations-v3',
+                #debug=True,
+            )
